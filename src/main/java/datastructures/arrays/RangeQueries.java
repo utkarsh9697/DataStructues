f76d80dc1d677ec879e53f14f7a4ae3bf9520024
utkarsh9697 <utkarsh.yadav@nineleaps.com>
2020-02-27 18:24:10 +0530

    Utkarsh | Range Queries |  Added solution for finding sum of range from query array on data input array

--- src/main/java/datastructures/arrays/RangeQueries.java
+++ src/main/java/datastructures/arrays/RangeQueries.java
@@ -1,4 +1,41 @@
 package datastructures.arrays;
 
 public class RangeQueries {
+
+    public static final int CONSTANT_VALUE_ZERO = 0;
+    public static final int CONSTANT_VALUE_ONE = 1;
+
+    public static void main(String[] args) {
+
+        // Input data array on which sum of value is to be found
+        int[] inputDataArray = {1, 1, 2, 1, 3, 4, 5, 2, 8};
+
+        // Query array providing the range of upper and lower bounds for sum calculation on input data array
+        int[][] inputQueryArray = {
+                {0, 4},
+                {1, 3},
+                {2, 4}
+        };
+
+        printSumOfValuesInQueryRange(inputDataArray, inputQueryArray);
+    }
+
+    /**
+     * This method is used to find the sum of every query range in data array from the provided query array.
+     * Array values have been hard coded for implementation but solution can be extended for dynamic solutions.
+     */
+    private static void printSumOfValuesInQueryRange(int[] inputDataArray, int[][] inputQueryArray) {
+        for (int i = CONSTANT_VALUE_ZERO; i < inputQueryArray.length; i++) {
+
+            int requiredRangeSum = CONSTANT_VALUE_ZERO;
+            int lowerBoundValue = inputQueryArray[i][CONSTANT_VALUE_ZERO];
+            int upperBoundValue = inputQueryArray[i][inputQueryArray[i].length - CONSTANT_VALUE_ONE];
+
+            for (int k = lowerBoundValue; k <= upperBoundValue; k++) {
+                requiredRangeSum = requiredRangeSum + inputDataArray[k];
+            }
+
+            System.out.println(requiredRangeSum);
+        }
+    }
 }
