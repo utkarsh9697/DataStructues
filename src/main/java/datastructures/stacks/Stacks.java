5afa0621a0ea76dd9ff69e421d9a399f392a02c5
utkarsh9697 <utkarsh.yadav@nineleaps.com>
2020-03-03 19:09:29 +0530

    Utkarsh | Stacks | Added implementation for basic stack operations

---
+++ src/main/java/datastructures/stacks/Stacks.java
@@ -0,0 +1,39 @@
+package datastructures.stacks;
+
+import java.util.Stack;
+
+public class Stacks {
+
+    public static void main(String[] args) {
+        implementStacks();
+    }
+
+    /**
+     * This method is used to show stack implementation for push, pop, peek ,search and empty methods.
+     */
+    private static void implementStacks() {
+        Stack<Integer> stack = new Stack<>();
+
+        // Pushing values in stack. Also using peek to see element at the top of the stack
+        for (int i = 0; i < 4; i++) {
+            stack.push(i);
+            System.out.println("Added value in stack is " + stack.peek());
+        }
+
+        // Searches value to be present in stack. Value defaults to -1 for not found
+        if (stack.search(1) != -1) {
+            System.out.println("Element found in stack");
+        }
+
+        // Popping elements until stack is empty
+        while (!stack.empty()) {
+            System.out.println("Element being popped out from stack is " + stack.peek());
+            stack.pop();
+        }
+
+        // Displaying stack empty message after popping all elements
+        if(stack.empty()){
+            System.out.println("Stack is empty now");
+        }
+    }
+}
